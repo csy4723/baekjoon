703da6b9bc3b0d8744c6c8ba57c14e4ddd3bb24e
csy4723 <96081821+csy4723@users.noreply.github.com>
2022-02-23 00:02:27 +0900

    Add files via upload
    for문 끝남

--- for문.java
+++ for문.java
@@ -23,6 +23,103 @@ public class for문 {
 		speedplus();
 
 		tilN();
+
+		Nlit();
+
+		aplusbtwice();
+
+		halfstar();
+
+		reversestar();
+
+		smallthanx();
+
+	}
+
+	private static void reversestar() {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+
+			for (int j = 1; j <= (num - i); j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+
+	}
+
+	private static void smallthanx() {
+		Scanner sc = new Scanner(System.in);
+
+		String[] arr = sc.nextLine().split(" ");
+
+		int n = Integer.parseInt(arr[0]);
+		int x = Integer.parseInt(arr[1]);
+
+		int[] nrr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+
+			nrr[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < n; i++) {
+
+			if (nrr[i] < x) {
+				System.out.print(nrr[i] + " ");
+			}
+		}
+
+	}
+
+	private static void halfstar() {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print("*");
+			}
+			System.out.println();
+		} // TODO Auto-generated method stub
+
+	}
+
+	private static void aplusbtwice() {
+		Scanner sc = new Scanner(System.in);
+
+		int test = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 1; i <= test; i++) {
+			String[] arr = sc.nextLine().split(" ");
+
+			int num1 = Integer.parseInt(arr[0]);
+			int num2 = Integer.parseInt(arr[1]);
+
+			System.out.println("Case #" + i + ": " + num1 + " + " + num2 + " = " + (num1 + num2));
+		}
+
+	}
+
+	private static void Nlit() {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+
+		for (int i = num; i > 0; i--) {
+			System.out.println(i);
+		}
+
 	}
 
 	private static void tilN() throws NumberFormatException, IOException {

22360eea6a457c0622b772f4f9297e3992dc5a7f
csy4723 <96081821+csy4723@users.noreply.github.com>
2022-03-13 19:52:29 +0900

    Add files via upload
    단어 개수 세는 법(+3)

--- 문자열.java
+++ 문자열.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class 문자열 {
 
@@ -15,8 +16,90 @@ public class 문자열 {
 //		repeatstr();
 //		maxcharat();
 		maxcharatOther();
+		blankspase();
+		blankspase_1();
+		blankspase_2();
 
 	}
+
+	private static void blankspase_2() throws IOException {
+		//원시 입력 형태로 문자를 하나씩 읽어들이기 
+		int count = 0;
+		int pre_str = 32;	// 공백을 의미한다.
+		int str ;
+		
+		
+		while(true) {
+			str = System.in.read();
+            
+			// 입력받은 문자가 공백일 때,
+			if(str == 32) {
+				// 이전의 문자가 공백이 아니면
+				if(pre_str != 32) count++;
+			}
+ 
+			// 입력받은 문자가 개행일때 ('\n')
+			else if(str == 10) {
+				// 이전의 문자가 공백이 아니면
+				if(pre_str != 32) count++;
+				break;
+			}
+			
+			pre_str = str;
+			
+		}
+		
+		System.out.println(count);
+	}
+ 
+		
+	
+
+	private static void blankspase_1() {
+		//StringTokenizer로 하는 법
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		
+		sc.close();
+		
+		StringTokenizer st = new StringTokenizer(s, " ");
+		
+		System.out.println(st.countTokens());//토큰의 개수를 반환
+		
+	}
+
+	private static void blankspase() throws IOException {
+		//앞뒤 공백 없애는 함수 trim(), strip()
+		//split() 함수 문제 
+		//" "공백이 있는 문자열에 공백을 없앤 결과는 NULL 이 아니라 ""빈 문자열을 반환하게 된다  
+		//""빈 문자열을 공백으로 split할 경우 나눌 수 있는 공백이 없기 때문에 자기 자신을 반환한다. 
+		//""빈 문자열은 null이 아니고 ""로 인스턴스화 된 문자열이기 때문에 split한 결과를 담은 배열의 길이는 0이 아니라 1이 되게 된다.
+		// 때문에 존재하지 않는 문자열이 존재한다고 결과를 도출하게 된다. 주의해야 함 
+		
+		
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+				String[] arr = br.readLine().split(" ");
+				
+				int cnt = 0;
+				
+				for(int i = 0; i<arr.length; i++) {
+					if(arr[i].isBlank()) {
+					
+					}else {
+						cnt++;
+					}
+					
+					
+				}
+				
+				System.out.println(cnt);
+				
+			}
+
+	
 
 	private static void maxcharatOther() throws IOException {
 		int str = System.in.read(); // 문자열만 입력 받기에 간소화 가능 : 출력부분에서만 캐스팅

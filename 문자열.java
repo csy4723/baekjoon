2a58faf975f5bffb0f3a37704bb4cc5f31ef0b8e
csy4723 <96081821+csy4723@users.noreply.github.com>
2022-03-16 23:37:17 +0900

    Add files via upload
    문자열 문제 끝!!!

--- 문자열.java
+++ 문자열.java
@@ -21,151 +21,229 @@ public class 문자열 {
 		blankspase_2();
 		reverseString();
 		RINGRING();
+		Krwawhat();
+		groupString();
+
+	}
+
+	private static void groupString() throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(br.readLine());
+		String[] arr = new String[num];
+		boolean grstr = true;
+		int result = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			arr[i] = br.readLine();
+
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+
+			for (int j = 0; j < arr[i].length() - 1; j++) {
+
+				if (arr[i].charAt(j) != arr[i].charAt(j + 1)) {
+
+					for (int k = j + 2; k < arr[i].length(); k++) {
+
+						if (arr[i].charAt(j) == arr[i].charAt(k)) {
+							grstr = false;
+						}
+
+					}
+
+				}
+
+			}
+
+			result = grstr ? result + 1 : result;
+			grstr = true;
+
+		}
+
+		System.out.println(result);
+
+	}
+
+	private static void Krwawhat() throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+
+		str = str.replace("c=", "č");
+		str = str.replace("c-", "ć");
+		str = str.replace("dz=", "dž");
+		str = str.replace("d-", "đ");
+		str = str.replace("lj", "lj");
+		str = str.replace("s=", "š");
+		str = str.replace("z=", "ž");
+
+		int cnt = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (i < str.length() - 1) {
+
+				if (str.charAt(i) == 'd' && str.charAt(i + 1) == 'ž') {
+					cnt++;
+					i++;
+
+				} else if (str.charAt(i) == 'l' && str.charAt(i + 1) == 'j') {
+					cnt++;
+					i++;
+
+				} else if (str.charAt(i) == 'n' && str.charAt(i + 1) == 'j') {
+					cnt++;
+					i++;
+
+				} else {
+					cnt++;
+				}
+			} else {
+				cnt++;
+			}
+
+		}
+
+		System.out.println(cnt);
 
 	}
 
 	private static void RINGRING() throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String[] srr = new String[10];
 		char A = 'A';
-		
-		for(int i = 2; i<srr.length; i++) {
-			
-			if(i == 7 || i==9) {
-				srr[i] = ""+A++;
+
+		for (int i = 2; i < srr.length; i++) {
+
+			if (i == 7 || i == 9) {
+				srr[i] = "" + A++;
 				srr[i] += A++;
 				srr[i] += A++;
 				srr[i] += A++;
-				
-			}else {
-				srr[i] = ""+A++;
+
+			} else {
+				srr[i] = "" + A++;
 				srr[i] += A++;
 				srr[i] += A++;
-				
+
 			}
-			
+
 		}
-		
+
 		String[] srr2 = br.readLine().split("");
 		int scn = 0;
-		
-		for(int i = 2; i<srr.length; i++) {
-			for(int j = 0; j<srr2.length; j++) {
-				
-				if(srr[i].contains(srr2[j])) {
-					
-					scn+=i+1;
-					
+
+		for (int i = 2; i < srr.length; i++) {
+			for (int j = 0; j < srr2.length; j++) {
+
+				if (srr[i].contains(srr2[j])) {
+
+					scn += i + 1;
+
 				}
-				
-				
-				
+
 			}
 		}
-		
+
 		System.out.println(scn);
-		
-		
-	
-		
+
 	}
 
 	private static void reverseString() throws IOException {
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		String[] arr = br.readLine().split(" ");
-		
+
 		StringBuilder sb1 = new StringBuilder(arr[0]);
 		StringBuilder sb2 = new StringBuilder(arr[1]);
-		
+
 		int num1 = Integer.parseInt(sb1.reverse().toString());
 		int num2 = Integer.parseInt(sb2.reverse().toString());
-		
+
 		System.out.println(Math.max(num1, num2));
-		
-		
+
 	}
 
 	private static void blankspase_2() throws IOException {
-		//원시 입력 형태로 문자를 하나씩 읽어들이기 
+		// 원시 입력 형태로 문자를 하나씩 읽어들이기
 		int count = 0;
-		int pre_str = 32;	// 공백을 의미한다.
-		int str ;
-		
-		
-		while(true) {
+		int pre_str = 32; // 공백을 의미한다.
+		int str;
+
+		while (true) {
 			str = System.in.read();
-            
+
 			// 입력받은 문자가 공백일 때,
-			if(str == 32) {
+			if (str == 32) {
 				// 이전의 문자가 공백이 아니면
-				if(pre_str != 32) count++;
+				if (pre_str != 32)
+					count++;
 			}
- 
+
 			// 입력받은 문자가 개행일때 ('\n')
-			else if(str == 10) {
+			else if (str == 10) {
 				// 이전의 문자가 공백이 아니면
-				if(pre_str != 32) count++;
+				if (pre_str != 32)
+					count++;
 				break;
 			}
-			
+
 			pre_str = str;
-			
+
 		}
-		
+
 		System.out.println(count);
 	}
- 
-		
-	
 
 	private static void blankspase_1() {
-		//StringTokenizer로 하는 법
-		
+		// StringTokenizer로 하는 법
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		String s = sc.nextLine();
-		
+
 		sc.close();
-		
+
 		StringTokenizer st = new StringTokenizer(s, " ");
-		
-		System.out.println(st.countTokens());//토큰의 개수를 반환
-		
+
+		System.out.println(st.countTokens());// 토큰의 개수를 반환
+
 	}
 
 	private static void blankspase() throws IOException {
-		//앞뒤 공백 없애는 함수 trim(), strip()
-		//split() 함수 문제 
-		//" "공백이 있는 문자열에 공백을 없앤 결과는 NULL 이 아니라 ""빈 문자열을 반환하게 된다  
-		//""빈 문자열을 공백으로 split할 경우 나눌 수 있는 공백이 없기 때문에 자기 자신을 반환한다. 
-		//""빈 문자열은 null이 아니고 ""로 인스턴스화 된 문자열이기 때문에 split한 결과를 담은 배열의 길이는 0이 아니라 1이 되게 된다.
-		// 때문에 존재하지 않는 문자열이 존재한다고 결과를 도출하게 된다. 주의해야 함 
-		
-		
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				
-				String[] arr = br.readLine().split(" ");
-				
-				int cnt = 0;
-				
-				for(int i = 0; i<arr.length; i++) {
-					if(arr[i].isBlank()) {
-					
-					}else {
-						cnt++;
-					}
-					
-					
-				}
-				
-				System.out.println(cnt);
-				
+		// 앞뒤 공백 없애는 함수 trim(), strip()
+		// split() 함수 문제
+		// " "공백이 있는 문자열에 공백을 없앤 결과는 NULL 이 아니라 ""빈 문자열을 반환하게 된다
+		// ""빈 문자열을 공백으로 split할 경우 나눌 수 있는 공백이 없기 때문에 자기 자신을 반환한다.
+		// ""빈 문자열은 null이 아니고 ""로 인스턴스화 된 문자열이기 때문에 split한 결과를 담은 배열의 길이는 0이 아니라 1이 되게
+		// 된다.
+		// 때문에 존재하지 않는 문자열이 존재한다고 결과를 도출하게 된다. 주의해야 함
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] arr = br.readLine().split(" ");
+
+		int cnt = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].isBlank()) {
+
+			} else {
+				cnt++;
 			}
 
-	
+		}
+
+		System.out.println(cnt);
+
+	}
 
 	private static void maxcharatOther() throws IOException {
 		int str = System.in.read(); // 문자열만 입력 받기에 간소화 가능 : 출력부분에서만 캐스팅
@@ -182,18 +260,18 @@ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		}
 
 		int max = 0;
-		int ch = '?' - 'A';//63-65
+		int ch = '?' - 'A';// 63-65
 
 		for (int i = 0; i < 26; i++) {
 			if (arr[i] > max) {
 				max = arr[i];// 개수가 더 많으면 max에 담아줌
-				ch = i;//ch엔 인덱스 담아줌
+				ch = i;// ch엔 인덱스 담아줌
 			} else if (arr[i] == max) {
 				ch = '?' - 'A';// 같으면 ch에 2를 담아줌
 			}
 		}
 
-		System.out.print((char) (ch + 'A'));// 인덱스 +65로 해당 알파벳의 아스키코드를 받을 수 있다. 
+		System.out.print((char) (ch + 'A'));// 인덱스 +65로 해당 알파벳의 아스키코드를 받을 수 있다.
 
 	}
 

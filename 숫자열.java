8aa0a886db864bce04171274a8797f3f31cb637b
csy4723 <96081821+csy4723@users.noreply.github.com>
2022-03-21 22:38:00 +0900

    Add files via upload
    벌집문제 푸는 다른 방법. +등차수열은 곱해서 푼다... 더하지 말고...

--- 숫자열.java
+++ 숫자열.java
@@ -9,7 +9,32 @@ public class 숫자열 {
 	public static void main(String[] args) throws IOException {
 		breakEvenPoing();
 		beehive();
+		anotherBeehive();
 
+	}
+
+	private static void anotherBeehive() throws NumberFormatException, IOException {
+
+		 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		int count = 1; // 겹 수(최소 루트)
+		int range = 2;	// 범위 (최솟값 기준) 
+ 
+		if (N == 1) {
+			System.out.print(1);
+		}
+ 
+		else {
+			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
+				range = range + (6 * count);
+				count++;
+			}
+			System.out.print(count);
+		}
+		// TODO Auto-generated method stub
+		
 	}
 
 	private static void beehive() throws NumberFormatException, IOException {
